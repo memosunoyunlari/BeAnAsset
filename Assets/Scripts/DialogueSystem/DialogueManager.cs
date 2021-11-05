682e3cf4d898c33563efb2cf7047746ff1a12f7e
memosunoyunlari <93601245+memosunoyunlari@users.noreply.github.com>
2021-11-05 16:00:14 +0300

    smallstepbutastepindeed
    updates for cliffevent text animation system and input gathering text prompt

--- Assets/Scripts/DialogueSystem/DialogueManager.cs
+++ Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -6,10 +6,20 @@ using System.Collections;
 
 public class DialogueManager : MonoBehaviour
 {
-    public RectTransform canvasRectT;
-    public RectTransform textRectTransform;
-    public Transform player;
-
+    // press E : Inspect input prompt
+    public GameObject promptTextBox;
+    private bool pressed;
+    private bool assetIN;
+    private Vector3 promptOffset;
+    
+    
+    //for the conversion
+    public Transform transformPanelandText;
+    
+    // cliff event text anchor
+    public Transform cliffEventLocation;
+    
+    // offset for the anchor - if needed;
     private Vector2 offset;
     
     public GameObject panelandText;
@@ -17,7 +27,10 @@ public class DialogueManager : MonoBehaviour
     public AudioClip typingClip;
     public AudioSourceGroup audioSourceGroup;
 
-    
+    // to get players ability to move while the text flows.
+    public GameObject player;
+
+
 
     [TextArea]
     public string cliff;
@@ -28,27 +41,62 @@ public class DialogueManager : MonoBehaviour
 
     private DialogueVertexAnimator dialogueVertexAnimator;
     void Awake() {
-        panelandText.SetActive(false);
-        offset = new Vector2(0, 2f);
+
+        
         dialogueVertexAnimator = new DialogueVertexAnimator(textBox, audioSourceGroup);
+
         
+        pressed = false;
+        promptOffset = new Vector2(0, 5f);
+
+
     }
 
-    private void LateUpdate()
+    private void Update()
     {
-       Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(Camera.main, player.position);
-       textRectTransform.anchoredPosition = screenPoint - canvasRectT.sizeDelta / 2f + offset;
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {   
-        if ( collision.tag == "Trigger")
-        {
-            StartCoroutine(startcliffeventandendit());
-        }
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(cliffEventLocation.position);
+      
+        screenPos.y = (Screen.height - screenPos.y);
         
+        transformPanelandText.position = screenPos;
+        promptTextBox.transform.position = screenPos - promptOffset;
+
+        if (Input.GetKeyDown(KeyCode.E) && assetIN == true && pressed == false)
+        {
+            panelandText.SetActive(true);
+            pressed = true;
+            promptTextBox.SetActive(false);
+            PlayDialogue1();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && assetIN == true && pressed == true)
+        {
+            pressed = false;
+            promptTextBox.SetActive(true);
+            panelandText.SetActive(false);
+        }
     }
 
+    private void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if (collider2D.CompareTag("Trigger") && pressed == false)
+        {
+            promptTextBox.SetActive(true);
+            assetIN = true;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collider2D)
+    {
+        if (collider2D.CompareTag("Trigger") && pressed == false)
+        {
+            promptTextBox.SetActive(false);
+            assetIN = false;
+        }
+    }
+
+    
     private void PlayDialogue1() {
         PlayDialogue(cliff);
     }
